979c24246f1dca3b267984583fdd7b51bcb50422
yarikkk13 <yaroslav.mahas@gmail.com>
2021-07-09 16:25:57 +0300

    lesson 02 hw 28:00

---
+++ user/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from .views import MyView
+
+urlpatterns = [
+    path('', MyView.as_view())
+]
--- user/views.py
+++ user/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+
+from rest_framework.views import APIView
+
 
 # Create your views here.
+
+class MyView(APIView):
+    def get(self, *args, **kwargs):
+        return Response('hello from get')
+
+    def post(self, *args, **kwargs):
+        return Response('hello from post')
+
+    def put(self, *args, **kwargs):
+        return Response('hello from put')
+
+    def patch(self, *args, **kwargs):
+        return Response('hello from patch')
+
+    def delete(self, *args, **kwargs):
+        return Response('hello from delete')
