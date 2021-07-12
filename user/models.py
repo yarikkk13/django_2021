11a1c1b0eb0ca92f477dc7f7a4eef7129bc12915
yarikkk13 <yaroslav.mahas@gmail.com>
2021-07-12 14:55:12 +0300

    lesson 01 hw

--- user/models.py
+++ user/models.py
@@ -3,6 +3,9 @@ from django.db import models
 
 # Create your models here.
 class UserModel(models.Model):
+    class Meta:
+        db_table = 'users'
     name = models.CharField(max_length=20)
     userName = models.CharField(max_length=20)
     email = models.EmailField(max_length=40)
+    age = models.IntegerField()
